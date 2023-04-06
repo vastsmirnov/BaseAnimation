50ba9b2496476739354e2354d542e504d1dca016
Valeriy Smirnov <smirnov@atlasbiomed.com>
2023-04-06 16:04:50 +0300

    Revert "добавил анимации"
    This reverts commit f52f1a878ac4f6cf318ae2c86d27fa45f524c5b1.

--- app/src/main/java/com/example/baseanimation/AniamtionState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.baseanimation
-
-object AnimationState {
-    var isAlphaStarted: Boolean = false
-    var isScaleStarted: Boolean = false
-    var isTranslateStarted: Boolean = false
-    var isRotateStarted: Boolean = false
-}
