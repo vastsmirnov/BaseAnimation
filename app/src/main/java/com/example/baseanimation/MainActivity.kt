5b8e70899512656345060c83e01175cee085ce47
Valeriy Smirnov <smirnov@atlasbiomed.com>
2023-04-06 16:05:00 +0300

    Revert "добавил верстку и слушатели нажатий"
    This reverts commit 357a9ca099aec4de74a9fd44a167eb5d190ff06e.

--- app/src/main/java/com/example/baseanimation/MainActivity.kt
+++ app/src/main/java/com/example/baseanimation/MainActivity.kt
@@ -2,48 +2,10 @@ package com.example.baseanimation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
-import com.example.baseanimation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-
-        with(binding) {
-            alphaMb.setOnClickListener(onAlphaClickListener())
-            scaleMb.setOnClickListener(onScaleClickListener())
-            translateMb.setOnClickListener(onTranslateClickListener())
-            rotateMb.setOnClickListener(onRotateClickListener())
-        }
-
-        setContentView(binding.root)
-    }
-
-    private fun onAlphaClickListener(): View.OnClickListener {
-        return View.OnClickListener {
-            Toast.makeText(this, "Alpha", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun onScaleClickListener(): View.OnClickListener {
-        return View.OnClickListener {
-            Toast.makeText(this, "Scale", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun onTranslateClickListener(): View.OnClickListener {
-        return View.OnClickListener {
-            Toast.makeText(this, "Translate", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun onRotateClickListener(): View.OnClickListener {
-        return View.OnClickListener {
-            Toast.makeText(this, "Rotate", Toast.LENGTH_SHORT).show()
-        }
+        setContentView(R.layout.activity_main)
     }
 }
